be63539457f701e42602c2b0581727a0dccc7843
Yacine Hariche <yacinedz@MacBook-Yacine.local>
2024-03-01 11:25:55 +0100

    placement pion

--- script.js
+++ script.js
@@ -4,25 +4,27 @@ addEventListener("DOMContentLoaded", (event) =>
     {
         constructor()
         {
-            // var lignes = parseInt(prompt("Nombre de lignes"));
-            // var colonnes = parseInt(prompt("Nombre de colonnes"));
+            this.lignes = parseInt(prompt("Nombre de lignes"));
+            this.colonnes = parseInt(prompt("Nombre de colonnes"));
 
-            var lignes = 6;
-            var colonnes = 6;
+            // let lignes = 5;
+            // let colonnes = 5;
 
             var body = document.getElementsByTagName("body")[0];
             var table = document.createElement("table");
             var tableBody = document.createElement("tbody");
 
-            this.currentPlayer = 1;
+            this.joueur = 1;
 
-            for (var i = 0; i < lignes; i++)
+            for (var i = 0; i < this.lignes; i++)
             {
                 var ligne = document.createElement("tr");
 
-                for (var j = 0; j < colonnes; j++)
+                for (var j = 0; j < this.colonnes; j++)
                 {
                     var jeton = document.createElement("td");
+                    jeton.setAttribute('coordx', i);
+                    jeton.setAttribute('coordy', j);
                     jeton.style.backgroundColor = "white";
                     jeton.style.border = "8px solid blue";
                     jeton.style.width = "8em";
@@ -32,7 +34,23 @@ addEventListener("DOMContentLoaded", (event) =>
                     table.style.borderRadius = "30px";
                     table.style.border = "3px solid blue";
 
-                    jeton.addEventListener("click", this.handleCellClick.bind(this));
+                    jeton.addEventListener("click", this.PlacementJeton.bind(this));
+
+                    jeton.addEventListener("mouseover", function(event)
+                    {
+                        if (event.target.style.backgroundColor === "white")
+                        {
+                            event.target.style.backgroundColor = "lightgray";
+                        }
+                    });
+
+                    jeton.addEventListener("mouseout", function(event)
+                    {
+                        if (event.target.style.backgroundColor === "lightgray")
+                        {
+                            event.target.style.backgroundColor = "white";
+                        }
+                    });
 
                     ligne.appendChild(jeton);
                 }
@@ -45,26 +63,29 @@ addEventListener("DOMContentLoaded", (event) =>
             table.setAttribute("border", "2");
         }
 
-        handleCellClick(event)
+        PlacementJeton(event)
         {
-            if (this.currentPlayer === 1)
+            var colonne = event.target.getAttribute('coordy');
+
+            for (var i = this.lignes - 1; i >= 0; i--)
             {
-                if (event.target.style.backgroundColor == "white")
+                var token = document.querySelector('td[coordx="' + i + '"][coordy="' + colonne + '"]');
+                if (token.style.backgroundColor === "white" || token.style.backgroundColor === "lightgray")
                 {
-                    event.target.style.backgroundColor = "red";
-                    this.currentPlayer = 2;
-                }
-            }
-            else
-            {
-                if (event.target.style.backgroundColor == "white")
-                {
-                    event.target.style.backgroundColor = "yellow";
-                    this.currentPlayer = 1;
+                    if (this.joueur === 1)
+                    {
+                        token.style.backgroundColor = "red";
+                        this.joueur = 2;
+                    }
+                    else
+                    {
+                        token.style.backgroundColor = "yellow";
+                        this.joueur = 1;
+                    }
+                    break;
                 }
             }
         }
     }
-
     let obj = new Grille();
 });
